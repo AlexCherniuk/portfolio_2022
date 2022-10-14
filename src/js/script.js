6e697bdcf2287cb29d212887d3e8d938424de7f4
AlexCherniuk <60302722+AlexCherniuk@users.noreply.github.com>
2022-10-14 12:22:39 +0200

    finished_sections3_4

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,16 @@ const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+	menu.classList.add('active');
 });
 
-closeElem.addEventListener('click', ()=> {
-    menu.classList.remove('active')
+closeElem.addEventListener('click', () => {
+	menu.classList.remove('active')
 });
+
+const counter = document.querySelectorAll('.skills__raitings-counter'),
+	lines = document.querySelectorAll('.skills__raitings-line span');
+
+counter.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
+})
